6ef111f895a733447b62aa711acc3aba53dfc3ab
Andrew <vandenHoeven.andrew@gmail.com>
2015-07-16 16:40:41 -0400

    added extra spritesheet

--- TheGame.py
+++ TheGame.py
@@ -6,6 +6,16 @@ from utilities.spritesheet import spritesheet # for loading the spritesheet
 import os # for paths
 
 
+'''
+    -RULES-
+    TBD
+    
+    -CONTROLS- 
+    PLAYER1 arrow keys
+    PLAYER2 WASD
+     
+'''
+
 DIMENSIONS=(40,40) 
 SURFACE = pygame.display.set_mode((16*DIMENSIONS[0],16*DIMENSIONS[1])) # each tile is 16 pixels wide
 
@@ -16,9 +26,11 @@ clock = pygame.time.Clock()
 SURFACE.fill(pygame.Color('black'))
 
 ss = spritesheet(os.path.join('spritesheet/roguelikeSheet_transparent.png'))
+char_ss = spritesheet(os.path.join('spritesheet/roguelikeChar_transparent.png'))
 # Sprite is 16x16 pixels at location 0,0 in the file...
 # Load  images into an array, their transparent bit is (255, 255, 255)
 images = [ss.images_at([(17*j, 17*i, 16,16) for i in range(57)],colorkey=(255, 255, 255)) for j in range(36)]
+char_images = [char_ss.images_at([(17*j, 17*i, 16,16) for i in range(54)],colorkey=(255, 255, 255)) for j in range(12)]
 
 
 
@@ -52,17 +64,25 @@ class GameMap:
     def draw_tile(self,coordinate):
         #redraws the a single tile, blitting over players
         draw_image_to_coord(self.background_layer[coordinate[0]][coordinate[1]], coordinate)
-        if self.foreground_layer[coordinate[0]][coordinate[1]] != (-1,-1): draw_image_to_coord (self.foreground_layer[coordinate[0]][coordinate[1]], coordinate)
+        if self.foreground_layer[coordinate[0]][coordinate[1]][0] != -1: draw_image_to_coord (self.foreground_layer[coordinate[0]][coordinate[1]], coordinate)
     
     def is_passable(self,coordinate):
         #tests to see if the tile in question is passable
         return True
     
 class Player:
-    def __init__(self,location):
+    def __init__(self,location,appearance):
         self.location=location
+        self.appearance= appearance
+        ''' appearance is a length 6 tuple
+            body type : 0-3
+            pant type : 0-9  4 and 9 are for dresses
+            shirt type: guhhh
+            hair type: 
+            beard type:
+        '''
         self.health=10
-        self.item=0
+        self.item=[]
     
     def move(self,direction,distance,game_map):
         #moves the player in the direction desired if possible
@@ -78,7 +98,8 @@ class Player:
                 break
                 
     def draw_player(self):
-        pass
+        #draw the player and all their items        
+        draw_image_to_coord((0, self.appearance[0]), self.loaction, images_list=char_images) # draw the body
         
         
 
@@ -86,13 +107,13 @@ def add_coords(coord1,coord2):
     # adds two tuples of coordinates together keeping in mind map wrapping, returns a tuple
     return ((coord1[0]+coord2[0])%DIMENSIONS[0],(coord1[1]+coord2[1])%DIMENSIONS[1])
         
-def draw_image_to_coord(img_location, draw_location):
+def draw_image_to_coord(img_location, draw_location, images_list=images):
     '''Takes as input two tuple, this first being the location on the spritesheet of the image
     The second tuple is the coordinate of the location where the image should be blitted to on the screen
     both use 0,0 as the top left corner 
     '''
     blit_location=(draw_location[0]*16,draw_location[1]*16)
-    SURFACE.blit(images[img_location[0]][img_location[1]], blit_location)
+    SURFACE.blit(images_list[img_location[0]][img_location[1]], blit_location)
 
 
 def game_loop(game_map):   
@@ -100,12 +121,16 @@ def game_loop(game_map):
     game_map.draw_all()
     while True:
     # this is the main game loop
+        
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 pygame.quit()
                 sys.exit()
         
+        keys = pygame.key.get_pressed()  #checking pressed keys
         
+        if keys[pygame.K_d]:
+            pass
         
         
         pygame.display.flip() # this draws all the updates to the screen
