bfb1a8dfd191a18ee814ed881779f4212618ac11
Andrew <vandenHoeven.andrew@gmail.com>
2015-07-17 13:01:40 -0400

    added lots
    added animated tiles, interactive tiles, base npc class as well as more
    to the map

--- TheGame.py
+++ TheGame.py
@@ -40,8 +40,8 @@ char_images = [char_ss.images_at([(17*j, 17*i, 16,16) for i in range(12)],colork
 def main():
     game_map=GameMap()
     
-    player1=Player(location=(2,2),appearance=(0,0,0,0,0,0),id=1)
-    player2=Player(location=(10,30),appearance=(2,0,0,0,0,0),id=2)
+    player1=Player(location=(2,2),appearance=(6,0,0,80,20,0),id=1)
+    player2=Player(location=(10,30),appearance=(8,0,0,0,0,0),id=2)
     players=[player1,player2]
     
     status_bar=StatusBar(players)
@@ -63,9 +63,9 @@ class GameMap:
         self.foreground_layer= np.array([[(-1,-1)]*40]*40)
         self.interactive_layer= np.array([[((-1,-1),0)]*40]*40) #interactive layer also specifies spritesheet
         
-        self.interactive_layer[20][20]=((47,1),1)
+        self.interactive_layer[20][20]=((47,1),1) #axe spawn
 
-        self.interactive_layer[33][11]=((37,4),1)
+        self.interactive_layer[33][11]=((37,4),1) #shield spawn
         
         #trees, bushes and grasses
         list_trees = [(3,3),(7,5),(11,9),(33,29),(18,22),(32,22)]
@@ -82,7 +82,7 @@ class GameMap:
         list_tall_dg_tree_base=[(4,17),(30,32),(26,31),(21,9),(37,37)]
         list_tall_dg_tree_top=[add_coords(x,(0,-1)) for x in list_tall_dg_tree_base]
         list_tall_pointy_tree_base=[(16,36),(38,33),(22,34),(29,36),(28,35),(28,38),(27,36),
-                                    (26,35),(26,37),(27,38),(25,36),(25,34),(25,38),(23,37),(31,37),(26,38)]
+                                    (26,35),(27,38),(25,36),(25,34),(25,38),(23,37),(31,37),(26,38)]
         list_tall_pointy_tree_top=[add_coords(x,(0,-1)) for x in list_tall_pointy_tree_base]
         
         #camp stuff
@@ -99,6 +99,9 @@ class GameMap:
         list_lillypad1 = [(5,33),(18,13)]
         list_lillypad2 = [(15,8),(10,7)]
         list_water_rock1 = [(17,8)]
+        list_dock_right =[(8,33)]
+        list_dock_middle=[]
+        list_dock_left=[]
         
         #dirt
         list_dirt_top_left=[(24,13)]
@@ -115,9 +118,31 @@ class GameMap:
         list_path_bend_bottom_right=[(35,15)]
         list_path_bend_top_left=[(35,14)]
         
+        #house stuff
+        list_roof_top_left=[(11,28)]
+        list_roof_top_right=[(12,28)]
+        list_roof_middle_left=[(11,29)]
+        list_roof_middle_right=[(12,29)]
+        list_roof_bottom_left=[(11,30)]
+        list_roof_bottom_right=[(12,30)]
+        list_building_top_left=[]
+        list_building_top=[]
+        list_building_top_right=[]
+        list_building_middle_left=[(11,30),]
+        list_building_middle=[]
+        list_building_middle_right=[(12,30)]
+        list_building_bottom_left=[(11,31)]
+        list_building_bottom=[]
+        list_building_bottom_right=[(12,31)]
+        list_doors_right=[(12,31)]
+        list_doors_left=[(11,31)]
+        list_chimney=[(12,28)]
+        list_window1= []
         
         
-          
+        
+        
+        
         # water tiles
         list_water = [(5,34),(6,34),(7,34),(5,33),(6,33),(7,33),(5,32),(6,32),(7,32)] +[(15,8),(16,8),(17,7),(10,7)]+[(17,8+i) for i in range(1,4)]
         list_water_up_edge = [(5,31),(6,31),(7,31)] + [(12+i,7) for i in range(4)]+[(10,6)]
@@ -179,6 +204,48 @@ class GameMap:
         for i,j in list_wild_flower4:
         	self.foreground_layer[i][j]=(31,9)
         
+        
+        #house stuff
+        for i,j in list_roof_top_left:
+        	self.background_layer2[i][j]=(20,21)
+        for i,j in list_roof_top_right:
+        	self.background_layer2[i][j]=(21,21)
+        for i,j in list_roof_middle_left:
+        	self.background_layer2[i][j]=(20,22)
+        for i,j in list_roof_middle_right:
+        	self.background_layer2[i][j]=(21,22)
+        for i,j in list_roof_bottom_left:
+        	self.background_layer2[i][j]=(20,23)
+        for i,j in list_roof_bottom_right:
+        	self.background_layer2[i][j]=(21,23)
+        for i,j in list_building_top_left:
+        	self.background_layer[i][j]=(17,21)
+        for i,j in list_building_top:
+        	self.background_layer[i][j]=(18,21)
+        for i,j in list_building_top_right:
+        	self.background_layer[i][j]=(19,21)
+        for i,j in list_building_middle_left:
+        	self.background_layer[i][j]=(17,22)
+        for i,j in list_building_middle:
+        	self.background_layer[i][j]=(18,22)
+        for i,j in list_building_middle_right:
+        	self.background_layer[i][j]=(19,22)
+        for i,j in list_building_bottom_left:
+        	self.background_layer[i][j]=(17,23)
+        for i,j in list_building_bottom:
+        	self.background_layer[i][j]=(18,23)
+        for i,j in list_building_bottom_right:
+        	self.background_layer[i][j]=(19,23)
+        for i,j in list_doors_right:
+        	self.interactive_layer[i][j]=((29,6),0)
+        for i,j in list_doors_left:
+        	self.interactive_layer[i][j]=((28,6),0)
+        for i,j in list_chimney:
+        	self.foreground_layer[i][j]=(36,20)
+        for i,j in list_window1:
+        	self.foreground_layer[i][j]=(43,5)
+        
+        
         #dirt
         for i,j in list_dirt_top_left:
         	self.background_layer2[i][j]=(7,9)
@@ -230,6 +297,12 @@ class GameMap:
         	self.foreground_layer[i][j]=(25,11)
         for i,j in list_water_rock1:
         	self.foreground_layer[i][j]=(55,23)
+        for i,j in list_dock_right:
+        	self.foreground_layer[i][j]=(53,12)
+        for i,j in list_dock_middle:
+        	self.foreground_layer[i][j]=(53,13)
+        for i,j in list_dock_left:
+        	self.foreground_layer[i][j]=(53,14)
         
         #water tiles
         for i,j in list_water:
@@ -295,14 +368,15 @@ class GameMap:
     
     def interation_logic(self,coordinate,object_code,player,players,status_bar):
         #this might get tricky
-        if object_code==((47,1),1):
+        if object_code==((47,1),1): # axe
             #be careful when updating this due to the way cycling of spawn locations is done
-            axe_spawns=[(20,20),(35,30),(2,38) ] #MAP_PARAMS
+            axe_spawns=[(20,20),(35,30),(2,38),(8,2),(23,3),(11,34) ] #MAP_PARAMS
             
             self.interactive_layer[coordinate]=((-1,-1),0)
             self.draw_tile(coordinate)
             current_spawn= axe_spawns.index(coordinate)
-            new_spawn=axe_spawns[(current_spawn+random.randint(1,2))%3]
+            spawn_count=len(axe_spawns)
+            new_spawn=axe_spawns[(current_spawn+random.randint(1,spawn_count-1))%spawn_count]
             self.interactive_layer[new_spawn]=((47,1),1)
             
             self.draw_tile(new_spawn)
@@ -312,7 +386,7 @@ class GameMap:
             status_bar.update_values(players)
             status_bar.draw_all()
 
-        if object_code==((37,4),1):
+        if object_code==((37,4),1):# shield
             shield_spawns=[(33,11), (15,14), (39,2)]
             
             self.interactive_layer[coordinate]=((-1,-1),0)
@@ -322,23 +396,63 @@ class GameMap:
             self.interactive_layer[new_spawn2]=((37,4),1)
 
             self.draw_tile(new_spawn2)
-            if players[not(player.id-1)].item >= 3:
-                players[not(player.id-1)].item-=3
+            players[not(player.id-1)].item-=3
+            if players[not(player.id-1)].item < 0:
+                players[not(player.id-1)].item = 0
             status_bar.update_values(players)
             status_bar.draw_all()
+        
+        if object_code == ((29,6),0): # building doors right closed
+            self.interactive_layer[coordinate]=((29,8),0)
+            self.interactive_layer[add_coords(coordinate,(-1,0))]=((28,8),0)
+            self.draw_tile(coordinate)
+            self.draw_tile(add_coords(coordinate,(-1,0)))
+            
+        elif object_code == ((28,6),0): # building doors left closed
+            self.interactive_layer[coordinate]=((28,8),0)
+            self.interactive_layer[add_coords(coordinate,(1,0))]=((29,8),0)
+            self.draw_tile(coordinate)
+            self.draw_tile(add_coords(coordinate,(1,0)))
+        
+        elif object_code == ((29,8),0): # building doors right open
+            self.interactive_layer[coordinate]=((29,6),0)
+            self.interactive_layer[add_coords(coordinate,(-1,0))]=((28,6),0)
+            self.draw_tile(coordinate)
+            self.draw_tile(add_coords(coordinate,(-1,0)))
+            
+        elif object_code == ((28,8),0): # building doors left open
+            self.interactive_layer[coordinate]=((28,6),0)
+            self.interactive_layer[add_coords(coordinate,(1,0))]=((29,6),0)
+            self.draw_tile(coordinate)
+            self.draw_tile(add_coords(coordinate,(1,0)))
+        
+    def animate(self,counter):
+        #deals with animated tiles, should probably write it so that drawing only happens when necessary in the future
+        
+        #fire
+        if counter%30<15:
+            self.foreground_layer[(28,16)]=(15,8)
+        else: 
+            self.foreground_layer[(28,16)]=(14,8)
+        self.draw_tile((28,16))  
+        
 
-
+        
+        
     def is_passable(self,coordinate):
         #tests to see if the tile in question is passable
         listImpassable = [(13,9), (3,1), (3,0), (3,2), (2,1), 
                             (4,1), (2,0), (4,0), (2,2), (4,2), 
                             (24,10),(15,11),(15,10),(26,9),(26,10),
                             (22,9),(16,11),(16,10),
-                            (48,10),(48,11),(49,11),(49,10)]
+                            (48,10),(48,11),(49,11),(49,10)]+ [(17+i,21+j) for i in range(5) for j in range(3)]
+        list_passable= [(53,12+i) for i in range(3)]
         condition1=tuple(self.foreground_layer[coordinate[0]][coordinate[1]]) in listImpassable
         condition2=tuple(self.background_layer[coordinate[0]][coordinate[1]]) in listImpassable
         condition3=tuple(self.background_layer2[coordinate[0]][coordinate[1]]) in listImpassable
-        if  condition1 or condition2 or condition3:
+        condition4=tuple(self.foreground_layer[coordinate[0]][coordinate[1]]) in list_passable
+        
+        if  (condition1 or condition2 or condition3) and not condition4:
         	return False
         return True
     
@@ -349,9 +463,10 @@ class Player:
         ''' appearance is a length 6 tuple
             body type : 0-3
             pant type : 0-9  4 and 9 are for dresses
+            shoe type : 0-9  4 and 9 are for dresses
             shirt type: guhhh
             hair type: 
-            beard type:
+            hair2 (beard) type:
         '''
         self.health=10
         self.id= id
@@ -385,28 +500,33 @@ class Player:
     def draw_player(self):
         #draw the player and all their items        
         draw_image_to_coord((0, self.appearance[0]), self.location, images_list=char_images) # draw the body
+        '''draw_image_to_coord((3, self.appearance[1]), self.location, images_list=char_images)  #draw the pants
+        draw_image_to_coord((4, self.appearance[2]), self.location, images_list=char_images)  #draw the shoes
+        draw_image_to_coord((6+self.appearance[3]%12, self.appearance[3]/12), self.location, images_list=char_images)  #draw the shirt
+        draw_image_to_coord((19+self.appearance[4]%8, self.appearance[4]/8), self.location, images_list=char_images)  #draw the hair1
+        draw_image_to_coord((19+self.appearance[5]%8, self.appearance[5]/8), self.location, images_list=char_images)  #draw the hair2 TO_DO add in the white hair'''
+        
     
     def interact(self,game_map,status_bar,players):
         #interact with the map
-        for direction in [(i,j) for i in range(-1,2) for j in range(-1,2)]:
+        for direction in [(i-1,j-1) for i in range(3) for j in range(3)]:
             coordinate=add_coords(self.location,direction)
+            
             object= tuple(game_map.interactive_layer[coordinate])
             game_map.interation_logic(coordinate,object,self,players,status_bar) 
+            if object !=((-1,-1),0): break # this is for the doors not auto close themselves    
                 
-                
-                
-def debug_grid():
+class Npc:
+    def __init__(self,location,appearance,id,type):
+        self.location=location
+        self.appearance=appearance
+        self.id=id
+        self.type=type
     
-    for i in range(DIMENSIONS[0]):
-        pygame.draw.line(SURFACE,pygame.Color("black"),(16*i,0),(16*i,640))
-        pygame.draw.line(SURFACE,pygame.Color("black"),(0,16*i),(640,16*i))
-    for i in range(DIMENSIONS[0]):
-        for j in range(DIMENSIONS[1]):
-            label_x = myfont.render(str(i), 1,pygame.Color("black"))
-            label_y = myfont.render(str(j), 1,pygame.Color("black"))
-            SURFACE.blit(label_x, (16*i+2, 16*j-1))
-            SURFACE.blit(label_y, (16*i+2, 16*j+6))
-     
+    def draw_npc(self):
+        #draws the npc
+        draw_image_to_coord((1, self.appearance[0]), self.location, images_list=char_images) 
+    
 class StatusBar:
     def __init__(self,players):
         self.update_values(players)
@@ -414,11 +534,13 @@ class StatusBar:
 
         
 
-    def update_values(self, players):
+    def update_values(self,players):
     	self.player1_hp=players[0].health
         self.player2_hp=players[1].health
         self.item_durability1=players[0].item
         self.item_durability2=players[1].item
+        self.player1_appearance=players[0].appearance
+        self.player2_appearance=players[1].appearance
         
 
     def draw_all(self):
@@ -457,7 +579,9 @@ class StatusBar:
             draw_image_to_coord((34,29), (9,40))
         else: 
             draw_image_to_coord((34,30), (9,40))
-
+        
+            #--- portrait ---
+        draw_image_to_coord((0, self.player1_appearance[0]), (2,41), images_list=char_images) 
         
         # player2  ----------------------------------------      
             #--- health bar ----
@@ -489,7 +613,24 @@ class StatusBar:
             draw_image_to_coord((33,29), (30,40))
         else: 
             draw_image_to_coord((33,30), (30,40))
+            
+            #--- portrait ---
+        draw_image_to_coord((0, self.player2_appearance[0]), (37,41), images_list=char_images) 
 
+def debug_grid():
+    
+    for i in range(DIMENSIONS[0]):
+        pygame.draw.line(SURFACE,pygame.Color("black"),(16*i,0),(16*i,640))
+        pygame.draw.line(SURFACE,pygame.Color("black"),(0,16*i),(640,16*i))
+    for i in range(DIMENSIONS[0]):
+        for j in range(DIMENSIONS[1]):
+            label_x = myfont.render(str(i), 1,pygame.Color("black"))
+            label_y = myfont.render(str(j), 1,pygame.Color("black"))
+            SURFACE.blit(label_x, (16*i+2, 16*j-1))
+            SURFACE.blit(label_y, (16*i+2, 16*j+6))
+ 
+
+        
 def add_coords(coord1,coord2):
     # adds two tuples of coordinates together keeping in mind map wrapping, returns a tuple
     return ((coord1[0]+coord2[0])%DIMENSIONS[0],(coord1[1]+coord2[1])%DIMENSIONS[1])
@@ -508,6 +649,11 @@ def game_loop(game_map,players,status_bar):
     #this function contain the main game loop
     game_map.draw_all()
     status_bar.draw_all()
+    
+    p1_cooldown=10
+    p2_cooldown=10
+    counter=0
+    
     while True:
         if DEBUG:debug_grid()
     # this is the main game loop
@@ -536,13 +682,15 @@ def game_loop(game_map,players,status_bar):
             elif keys[pygame.K_a]: direction= (-1,0)
             players[1].move(direction,1,game_map,players)
 
-        if keys[pygame.K_SLASH]:
+        if keys[pygame.K_SLASH] and p1_cooldown<0:
         	#player1's interact
-        	players[0].interact(game_map,status_bar,players)
+            players[0].interact(game_map,status_bar,players)
+            p1_cooldown=30
 
-        if keys[pygame.K_q]:
+        if keys[pygame.K_q] and p2_cooldown<0:
         	#player2's interact
-        	players[1].interact(game_map,status_bar,players)
+            players[1].interact(game_map,status_bar,players)
+            p2_cooldown=30
 
         if keys[pygame.K_PERIOD]:
         	#player1's attack
@@ -553,7 +701,9 @@ def game_loop(game_map,players,status_bar):
         	players[1].damage(players, status_bar)
 
 
-            
+        #animations
+        counter=(counter+1)%150
+        game_map.animate(counter)    
         
         for player in players: 
             #redraw the players each frame
@@ -562,19 +712,23 @@ def game_loop(game_map,players,status_bar):
             #check for victory
             break
         
+        
+        p1_cooldown-=1
+        p2_cooldown-=1
+        
+        
         pygame.display.flip() # this draws all the updates to the screen
         clock.tick(FPS) 
     
     myfont = pygame.font.SysFont("monospace", 10)
     if players[0].health==0:
         label= myfont.render("PLAYER 2 WINS", 1,pygame.Color("black"))
-        pygame.draw.rect(SURFACE,pygame.Color("white"),(220,240,200,40))
-        SURFACE.blit(label, (280,255))
     else:   
         label= myfont.render("PLAYER 1 WINS", 1,pygame.Color("black"))
-        pygame.draw.rect(SURFACE,pygame.Color("white"),(220,240,200,40))
-        SURFACE.blit(label, (280,255))
-        pygame.display.flip() # this draws all the updates to the screen
+    pygame.draw.rect(SURFACE,pygame.Color("white"),(220,240,200,40))
+    pygame.draw.rect(SURFACE,pygame.Color("black"),(220,240,200,40),2)
+    SURFACE.blit(label, (280,255))
+    pygame.display.flip() # this draws all the updates to the screen
     while True:
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
